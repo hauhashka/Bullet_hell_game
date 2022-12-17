7ee9748d9e20e38e0eb8fc2615f78643f4e6617a
hauhashka <hauhashka@mail.ru>
2022-12-17 11:58:24 +0300

    based+

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 import pygame
+import os
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+class Background(pygame.sprite.Sprite):
+    girl = load_image('girl_2.jpg')
+    girl = pygame.transform.scale(girl, (600, 900))
+
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = Background.girl
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
+
+
+all_sprites = pygame.sprite.Group()
+boackground = Background(all_sprites)
+
 
 if __name__ == '__main__':
     pygame.init()
@@ -14,3 +43,8 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        pygame.display.flip()
+        all_sprites.draw(screen)
+        clock.tick(fps)
+        ticks += 1
+
