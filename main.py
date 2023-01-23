cf8cdac8a56e767ff6c178f58a4dce09d9cab4a6
hauhashka <hauhashka@mail.ru>
2023-01-23 20:55:31 +0300

    finally pre final version

--- main.py
+++ main.py
@@ -57,55 +57,55 @@ class ButtonLevel1(pygame.sprite.Sprite):
 
 
 class ButtonLevel2(pygame.sprite.Sprite):
-    cur = con.cursor()
-    level2_available = cur.execute('SELECT passed FROM WL '
-                                   'WHERE level like "1"').fetchall()
-            level2_images = (load_image('lvl2_en.png'), load_image('lvl2_dis.png'))
 
     def __init__(self, *group):
         super().__init__(*group)
-        self.image = ButtonLevel2.image
+        cur = con.cursor()
+        self.level2_available = cur.execute('SELECT passed FROM WL '
+                                       'WHERE level like "1"').fetchall()
+        level2_images = (load_image('lv2_en.png'), load_image('lv2_dis.png'))
+        if self.level2_available[0][0]:
+            self.image = level2_images[0]
+        else:
+            self.image = level2_images[1]
         self.rect = self.image.get_rect()
         self.rect.x = 90
         self.rect.y = 450
         self.pos = (200, 100)
-        if level2_available[0][0]:
-            image = load_image('lv2_en.png')
-        else:
-            image = load_image('lv2_dis.png')
+
 
     def update(self, *args):
         if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
-                self.rect.collidepoint(args[0].pos) and ButtonLevel2.level2_available[0][0]:
+                self.rect.collidepoint(args[0].pos) and self.level2_available[0][0]:
             load_level(2)
-        if level2_available[0][0]:
-            image = load_image('lv2_en.png')
-        else:
-            image = load_image('lv2_dis.png')
 
 
 class ButtonLevel3(pygame.sprite.Sprite):
     cur = con.cursor()
     level3_available = cur.execute('SELECT passed FROM WL '
                                    'WHERE level like "2"').fetchall()
-    if level3_available[0][0]:
-        image = load_image('lv3_en.png')
-    else:
-        image = load_image('lv3_dis.png')
+    level3_images = (load_image('lv3_en.png'), load_image('lv3_dis.png'))
 
     def __init__(self, *group):
         super().__init__(*group)
-        self.image = ButtonLevel3.image
+        cur = con.cursor()
+        self.level3_available = cur.execute('SELECT passed FROM WL '
+                                       'WHERE level like "2"').fetchall()
+        level3_images = (load_image('lv3_en.png'), load_image('lv3_dis.png'))
+        if self.level3_available[0][0]:
+            self.image = level3_images[0]
+        else:
+            self.image = level3_images[1]
         self.rect = self.image.get_rect()
         self.rect.x = 90
         self.rect.y = 550
         self.pos = (200, 100)
 
+
     def update(self, *args):
         if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
-                self.rect.collidepoint(args[0].pos) and ButtonLevel3.level3_available[0][0]:
+                self.rect.collidepoint(args[0].pos) and self.level3_available[0][0]:
             load_level(3)
-
 
 class Background(pygame.sprite.Sprite):
     girl = load_image('girl_1.1.jpg')
